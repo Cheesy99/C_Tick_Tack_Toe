7567317c06161fb19bef9de349291d98affbd799
Tobias Chisi <tobiaschisi@gmail.com>
2024-03-25 20:35:07 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMOve()
+char checkWinner();
+void
+
+
+int main() {
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
